6f06dbeaab02b3084aac34f83636660aea734b94
joshua <zaitochegne@gmail.com>
2024-10-14 12:47:27 -0500

    creando component NavDashboard  y el remplazando el html nav anterior y agregandolo al nuevo componente

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,36 +1,41 @@
-import { Outlet,Link ,useNavigate} from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import styleDashboard from "./style/dashboard.module.css"
-function Dashboard(){
-    const {logout} = useAuth();
-    const navigate = useNavigate();
-    return (<>
-        <div className={styleDashboard.dashboardContainer}>
+function Dashboard() {
+
+  return (<>
+    <div className={styleDashboard.dashboardContainer}>
       <header className={styleDashboard.header}>
         <h1>Soy human</h1>
-        </header>
-      <nav className={styleDashboard.dashboardNav}>
-      <h1 id={styleDashboard.title}>Dashboard</h1>
-        <ul>
-        <li><Link to="playlist">playlist</Link></li>
-          <li><Link to="profile">Profile</Link></li>
-          <li><Link to="settings">Settings</Link></li>
-          <li
-          onClick={()=>{
-            logout();
-            navigate("/");
-          }}
-          >singout</li>
-        </ul>
-      </nav>
-
+      </header>
+      <NavDashboard/>
       {/* Aquí se renderizan las subrutas */}
       <Outlet />
       <div className={styleDashboard.reproductor}>
         <h1>Reproductor</h1>
       </div>
     </div>
-    </>)
+  </>)
 }
 
+const NavDashboard = () => {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+  return (
+    <nav className={styleDashboard.dashboardNav}>
+      <h1 id={styleDashboard.title}>Dashboard</h1>
+      <ul>
+        <li><Link to="playlist">playlist</Link></li>
+        <li><Link to="profile">Profile</Link></li>
+        <li><Link to="settings">Settings</Link></li>
+        <li
+          onClick={() => {
+            logout();
+            navigate("/");
+          }}
+        >singout</li>
+      </ul>
+    </nav>
+  )
+}
 export default Dashboard;
